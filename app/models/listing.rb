11726472d06bf95b2a13516fb204aa7cbefe9022
matt curtright <matt.curtright@gmail.com>
2014-12-29 14:08:56 -0600

    associate listings with users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -8,4 +8,6 @@ validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   validates :name, :description, :price, presence: true
   validates :price, numericality: { greater_than: 0 }
+
+  belongs_to :user
 end
